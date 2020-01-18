443db1c94f3eb5d72f5aa7a9552308595472bd8e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-18 08:09:49 +0100

    Summon gradle

---
+++ src/main/java/bomberman/BombermanMod.java
@@ -0,0 +1,15 @@
+package bomberman;
+
+import mindustry.game.*;
+import mindustry.plugin.*;
+
+import static arc.util.Log.info;
+
+public class BombermanMod extends Plugin{
+    private final Rules rules = new Rules();
+
+    @Override
+    public void init(){
+        info("senpai bomberman <3");
+    }
+}
