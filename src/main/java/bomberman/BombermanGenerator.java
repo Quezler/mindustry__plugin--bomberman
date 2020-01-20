51a6c0069fbde3b5fff82d4e2269cdc8c5f41343
J-VdS <jeroen.vanderstraeten@ugent.be>
2020-01-20 16:01:37 +0100

    first changes

--- src/main/java/bomberman/BombermanGenerator.java
+++ src/main/java/bomberman/BombermanGenerator.java
@@ -12,13 +12,19 @@ import mindustry.world.blocks.*;
 import mindustry.maps.generators.*;
 
 import static mindustry.Vars.world;
+//debugging
+import static arc.util.Log.info;
 
 public class BombermanGenerator extends Generator{
+    public final static Team blockteam = Team.derelict;//Team.blue;
     public final static int worldborder = 3;
     public final static int grid = 11;
     public final static int size = (grid * 3) + (worldborder * 2);
 
     public Array<Tile> breakable = new Array<>();
+    public ArrayMap<Tile, Powerup> pWalls = new ArrayMap<>();
+    //2D matrix
+    public final int[][] spawns = {{34, 34}, {4, 4}, {34, 4}, {4, 34}};
 
     BombermanGenerator(){
         super(size, size);
@@ -54,23 +60,29 @@ public class BombermanGenerator extends Generator{
         }
 
         // scrap
-
-//        for(int x = worldborder; x < width - worldborder; x++){
-//            for(int y = worldborder; y < height - worldborder; y++){
-//
-//                if(((y / 3) % 2) == 1 && (x % 3) == 1 && ((x / 3) % 2) == 1 && (y % 3) == 1) tiles[x][y].setBlock(Blocks.scrapWall);
-//            }
-//        }
-
-        // scrap
         breakable.clear();
+        //fix with arc type
+        int counter = 0;
         for(int x = worldborder; x < width - worldborder; x++){
             for(int y = worldborder; y < height - worldborder; y++){
+                //remove corners
+                //ugly notation
+                if (
+                        (x == worldborder + 1 && y == worldborder + 1) ||
+                        (x == worldborder + 1 && y == height - worldborder - 2) ||
+                        (x == width - worldborder - 2 && y == worldborder + 1) ||
+                        (x == width - worldborder - 2 && y == height - worldborder - 2)){
+                    //TODO: fancy floor
+                    //fill spawn array/matrix
+                    continue;
+                }
 
                 if((x % 3) != 1 || (y % 3) != 1) continue;
                 if(tiles[x][y].block() != Blocks.air) continue;
                 breakable.add(tiles[x][y]);
-                tiles[x][y].set(Blocks.scrapWallHuge, Team.derelict);
+
+                //team choice is important make sure no one uses this team !
+                tiles[x][y].set(Blocks.scrapWallHuge, blockteam);
             }
         }
 
@@ -78,7 +90,9 @@ public class BombermanGenerator extends Generator{
         for(Powerup powerup : Powerup.values()){
             Tile tmp = breakable.random();
             tmp.remove();
+            //team doesn't matter now
             tmp.set(powerup.block, Team.derelict);
+            pWalls.put(tmp, powerup);
         }
 
         world.setMap(new Map(StringMap.of("name", "Bomberman")));
--- src/main/java/bomberman/BombermanMod.java
+++ src/main/java/bomberman/BombermanMod.java
@@ -3,8 +3,10 @@ package bomberman;
 import arc.*;
 import arc.struct.*;
 import arc.util.*;
+import mindustry.Vars;
 import mindustry.content.*;
 import mindustry.entities.effect.*;
+import mindustry.entities.type.Player;
 import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.plugin.*;
@@ -20,6 +22,9 @@ import static arc.util.Log.info;
 public class BombermanMod extends Plugin{
     private final Rules rules = new Rules();
 
+    //remove
+    private int counter = 0;
+
     private BombermanGenerator generator;
 
     @Override
@@ -29,47 +34,79 @@ public class BombermanMod extends Plugin{
         rules.infiniteResources = true;
         rules.canGameOver = false;
 
+        //Todo: check for min 2 players and have a countdown (~ 10 seconds)
+        //if game is already running -> spectator mode
         Events.on(PlayerJoin.class, event -> {
             if(!active()) return;
 
             event.player.kill();
             event.player.setTeam(Team.sharded);
             event.player.dead = false;
-            event.player.set(5, 5);
+
+            //set location
+            Call.onPositionSet(event.player.con, generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
+            //event.player.set(34*8, 34*8);
+            event.player.setNet(generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
+            event.player.set(generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
+            //this.counter++;
+            //player
             event.player.mech = Powerup.yellow.mech;
             event.player.heal();
         });
 
-        // fixme: break event does not include destroyed block
-        Events.on(BlockBuildEndEvent.class, event -> {
-            if(event.breaking) return; // fixme: invert ^
-
-            Powerup tmp = Powerup.wall(event.tile.block());
-            if(tmp == null) return;
-
-            Log.info(event.player.mech);
-            event.player.mech = tmp.mech;
+        //block flying over walls
+        Events.on(Trigger.update, () -> {
+            for (Player p: playerGroup){
+                if (p.getTeam() != Team.sharded) continue;
+                if (world.tile(p.tileX(), p.tileY()).block() != Blocks.air){
+                    p.sendMessage("[scarlet]FLYING OVER WALLS == CHEATING\ndisqualified!");
+                    p.setTeam(Team.green);
+                    Call.onPlayerDeath(p);
+                }
+            }
         });
 
+        //deconstruct wall
+        Events.on(BuildSelectEvent.class, event -> {
+            if(!event.breaking) return;
+
+            //could be null
+            Powerup tmp = generator.pWalls.removeKey(event.tile);
+            if(tmp == null) return;
+
+            Player player = (Player) event.builder;
+            Log.info(player.mech);
+            player.mech = tmp.mech;
+
+        });
+        //build reactor
         Events.on(BlockBuildEndEvent.class, event -> {
             if(event.breaking) return;
 
-            if(event.tile.block() != Blocks.thoriumReactor) return;
-
-            Call.transferItemTo(Items.thorium, 7, event.player.x, event.player.y, event.tile);
+            if(event.tile.block() == Blocks.thoriumReactor) {
+                //explode nuke
+                Call.transferItemTo(Items.thorium, 7, event.player.x, event.player.y, event.tile);
+            }
         });
 
+        //destroys the walls
         Events.on(BlockDestroyEvent.class, event -> {
             if(event.tile.block() != Blocks.thoriumReactor) return;
+            //TODO herschrijven
             generator.breakable.each(tile -> {
+
                 if(tile.block() != Blocks.scrapWallHuge) return;
                 if(event.tile.x == tile.x || event.tile.y == tile.y){
-                    tile.removeNet();
+                    /*new method - not in servertestfile
+                    tile.removeNet();*/
+                    //alternative
+                    Time.run(0f, tile.entity::kill);
                     tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
                 }
             });
+            //TODO: check if player was in laser/fire
         });
-
+        //what does it do
         netServer.assigner = (player, players) -> Team.sharded;
     }
 
@@ -83,6 +120,13 @@ public class BombermanMod extends Plugin{
             state.rules = rules.copy();
             logic.play();
             netServer.openServer();
+        });
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler){
+        handler.<Player>register("powerups", "info about the powerups.", (args, player) -> {
+            player.sendMessage("blaljajkjkdlqjfkljkjkdjfkljfksjlj");
         });
     }
 
