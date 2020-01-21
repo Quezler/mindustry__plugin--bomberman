aef99d9ba1b6b77139233c7e8ef6e0b0d6eea36e
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-21 11:01:25 +0100

    Rewrite world generator

--- src/main/java/bomberman/BombermanMod.java
+++ src/main/java/bomberman/BombermanMod.java
@@ -1,27 +1,23 @@
 package bomberman;
 
 import arc.*;
-import arc.struct.*;
 import arc.util.*;
-import mindustry.content.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.type.Player;
-import mindustry.game.*;
 import mindustry.gen.*;
+import mindustry.type.*;
+import mindustry.game.*;
+import mindustry.world.*;
 import mindustry.plugin.*;
+import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.core.GameState.*;
-import mindustry.type.*;
-import mindustry.world.*;
+import mindustry.entities.type.Player;
+import mindustry.world.blocks.BuildBlock.*;
 
 import static mindustry.Vars.*;
 import static arc.util.Log.info;
 
 public class BombermanMod extends Plugin{
     private final Rules rules = new Rules();
-
-    //remove
-    private int counter = 0;
 
     private BombermanGenerator generator;
 
@@ -31,8 +27,6 @@ public class BombermanMod extends Plugin{
         rules.tags.put("bomberman", "true");
         rules.infiniteResources = true;
         rules.canGameOver = false;
-        //test
-        rules.buildSpeedMultiplier = 0.5f;
 
         //Todo: check for min 2 players and have a countdown (~ 10 seconds)
         //if game is already running -> spectator mode
@@ -44,12 +38,10 @@ public class BombermanMod extends Plugin{
             event.player.dead = false;
 
             //set location
-            Call.onPositionSet(event.player.con, generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
-            //event.player.set(34*8, 34*8);
-            event.player.setNet(generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
-            event.player.set(generator.spawns[counter][0]*8, generator.spawns[counter][1]*8);
-            //this.counter++;
-            //player
+            Call.onPositionSet(event.player.con, generator.spawns[0][0]*8, generator.spawns[0][1]*8);
+            event.player.setNet(generator.spawns[0][0]*8, generator.spawns[0][1]*8);
+            event.player.set(generator.spawns[0][0]*8, generator.spawns[0][1]*8);
+
             event.player.mech = Powerup.copper.mech;
             event.player.heal();
         });
@@ -58,28 +50,15 @@ public class BombermanMod extends Plugin{
         Events.on(Trigger.update, () -> {
             for (Player p: playerGroup){
                 if (p.getTeam() != Team.sharded) continue;
-                if (world.tile(p.tileX(), p.tileY()).block() != Blocks.air){
-                    Call.sendMessage("[scarlet]FLYING OVER WALLS == CHEATING\ndisqualified!");
-                    p.setTeam(Team.green);
-                    Call.onPlayerDeath(p);
+                if (world.tile(p.tileX(), p.tileY()) != null && world.tile(p.tileX(), p.tileY()).block() != Blocks.air){
+//                    Call.sendMessage("[scarlet]FLYING OVER WALLS == CHEATING\ndisqualified!");
+//                    p.setTeam(Team.green);
+//                    Call.onPlayerDeath(p);
                 }
             }
         });
 
-        //deconstruct wall
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking) return;
-
-            //could be null
-            Powerup tmp = generator.pWalls.removeKey(event.tile);
-            if(tmp == null) return;
-
-            Player player = (Player) event.builder;
-            Log.info(player.mech);
-            player.mech = tmp.mech;
-
-        });
-        //build reactor
+        // construct reactor
         Events.on(BlockBuildEndEvent.class, event -> {
             if(event.breaking) return;
 
@@ -90,78 +69,90 @@ public class BombermanMod extends Plugin{
             }
         });
 
-        //destroys the walls
-        Events.on(BlockDestroyEvent.class, event -> {
-            if(event.tile.block() != Blocks.thoriumReactor) return;
-            int x = event.tile.x;
-            int y = event.tile.y;
-            //delete horizontal
-            boolean hdamage = false;
-            if (!(world.tile(x-3, y).block() == generator.pallete.wall && world.tile(x+3, y).block() == generator.pallete.wall)){
-                hdamage = true;
-                generator.breakable.each(tile -> {
-                    if(tile.block() != generator.pallete.blockade) return;
-                    if(event.tile.y == tile.y) {
-                        /*new method - not in servertestfile
-                        tile.removeNet();*/
-                        //alternative
-                        Time.run(0f, tile.entity::kill);
-                        //TODO: draw laser on airtiles
-                        tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
-                    }
-                });
-            }
-            //delete vertical
-            boolean vdamage = false;
-            if (!(world.tile(x, y-3).block() == generator.pallete.wall && world.tile(x, y+3).block() == generator.pallete.wall)){
-                vdamage = true;
-                generator.breakable.each(tile -> {
-                    if(tile.block() != generator.pallete.blockade) return;
-                    if(event.tile.x == tile.x) {
-                        /*new method - not in servertestfile
-                        tile.removeNet();*/
-                        //alternative
-                        Time.run(0f, tile.entity::kill);
-                        tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
-                    }
-                });
-            }
-            /*
-            generator.breakable.each(tile -> {
+        // deconstruct wall
+        Events.on(BlockBuildEndEvent.class, event -> {
+            if(!event.breaking) return;
 
-                if(tile.block() != generator.pallete.blockade) return;
-                if(event.tile.x == tile.x || event.tile.y == tile.y){
-                    //new method - not in servertestfile
-                    //tile.removeNet();
-                    //alternative
-                    Time.run(0f, tile.entity::kill);
-                    tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
-                }
-            });*/
-            //check if player was in laser/fire
-            for (Player p: playerGroup){
-                //already dead
-                if (p.getTeam() != Team.sharded) continue;
-                if (vdamage){
-                    if (x-1 <= p.tileX() && p.tileX() <= x+1){
-                        //kill player and put on green team
-                        Call.sendMessage('\n' + p.name + "[sky] DIED");
-                        p.setTeam(Team.green);
-                        Call.onPlayerDeath(p);
-                        continue;
-                    }
-                }
-                if (hdamage){
-                    if (y-1 <= p.tileY() && p.tileY() <= y+1){
-                        //kill player and put on green team
-                        Call.sendMessage(p.name + "[sky] DIED");
-                        p.setTeam(Team.green);
-                        Call.onPlayerDeath(p);
-                    }
-                }
-            }
-            //TODO: restart condition
+            Powerup tmp = Powerup.wall( ((BuildEntity)event.tile.ent()).previous );
+            if(tmp == null) return;
+
+            event.player.mech = tmp.mech;
+            event.player.heal();
         });
+
+//        //destroys the walls
+//        Events.on(BlockDestroyEvent.class, event -> {
+//            if(event.tile.block() != Blocks.thoriumReactor) return;
+//            int x = event.tile.x;
+//            int y = event.tile.y;
+//            //delete horizontal
+//            boolean hdamage = false;
+//            if (!(world.tile(x-3, y).block() == generator.pallete.wall && world.tile(x+3, y).block() == generator.pallete.wall)){
+//                hdamage = true;
+//                generator.breakable.each(tile -> {
+//                    if(tile.block() != generator.pallete.blockade) return;
+//                    if(event.tile.y == tile.y) {
+//                        /*new method - not in servertestfile
+//                        tile.removeNet();*/
+//                        //alternative
+//                        Time.run(0f, tile.entity::kill);
+//                        //TODO: draw laser on airtiles
+//                        tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
+//                    }
+//                });
+//            }
+//            //delete vertical
+//            boolean vdamage = false;
+//            if (!(world.tile(x, y-3).block() == generator.pallete.wall && world.tile(x, y+3).block() == generator.pallete.wall)){
+//                vdamage = true;
+//                generator.breakable.each(tile -> {
+//                    if(tile.block() != generator.pallete.blockade) return;
+//                    if(event.tile.x == tile.x) {
+//                        /*new method - not in servertestfile
+//                        tile.removeNet();*/
+//                        //alternative
+//                        Time.run(0f, tile.entity::kill);
+//                        tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
+//                    }
+//                });
+//            }
+//            /*
+//            generator.breakable.each(tile -> {
+//
+//                if(tile.block() != generator.pallete.blockade) return;
+//                if(event.tile.x == tile.x || event.tile.y == tile.y){
+//                    //new method - not in servertestfile
+//                    //tile.removeNet();
+//                    //alternative
+//                    Time.run(0f, tile.entity::kill);
+//                    tile.getLinkedTilesAs(Blocks.scrapWallLarge, new Array<>()).each(Fire::create);
+//                }
+//            });*/
+//            //check if player was in laser/fire
+//            for (Player p: playerGroup){
+//                //already dead
+//                if (p.getTeam() != Team.sharded) continue;
+//                if (vdamage){
+//                    if (x-1 <= p.tileX() && p.tileX() <= x+1){
+//                        //kill player and put on green team
+//                        Call.sendMessage('\n' + p.name + "[sky] DIED");
+//                        p.setTeam(Team.green);
+//                        Call.onPlayerDeath(p);
+//                        continue;
+//                    }
+//                }
+//                if (hdamage){
+//                    if (y-1 <= p.tileY() && p.tileY() <= y+1){
+//                        //kill player and put on green team
+//                        Call.sendMessage(p.name + "[sky] DIED");
+//                        p.setTeam(Team.green);
+//                        Call.onPlayerDeath(p);
+//                    }
+//                }
+//            }
+//            //TODO: restart condition
+//        });
+
         //what does this do
         netServer.assigner = (player, players) -> Team.sharded;
     }
@@ -179,29 +170,22 @@ public class BombermanMod extends Plugin{
         });
     }
 
-    @Override
-    public void registerClientCommands(CommandHandler handler){
-        handler.<Player>register("powerups", "info about the powerups.", (args, player) -> {
-            player.sendMessage("blaljajkjkdlqjfkljkjkdjfkljfksjlj");
-        });
-    }
-
     public boolean active(){
         return state.rules.tags.getBool("bomberman") && !state.is(State.menu);
     }
 
     enum Powerup{
-        copper    (Mechs.alpha, Blocks.copperWall    ,  7),
-        titanium  (Mechs.delta, Blocks.titaniumWall  , 14),
-        plastanium(Mechs.tau  , Blocks.plastaniumWall, 12),
-        surge     (Mechs.omega, Blocks.surgeWall     , 20);
+        copper    (Mechs.alpha, Slate.State.copper    ,  7),
+        titanium  (Mechs.delta, Slate.State.titanium  , 14),
+        plastanium(Mechs.tau  , Slate.State.plastanium, 12),
+        surge     (Mechs.omega, Slate.State.surge     , 20);
 
 
         public final Mech mech;
-        public final Block block;
+        public final Slate.State block; // todo: rename/refractor to slate
         public final int thorium;
 
-        Powerup(Mech mech, Block block, int thorium){
+        Powerup(Mech mech, Slate.State block, int thorium){
             this.mech = mech;
             this.block = block;
             this.thorium = thorium;
@@ -209,7 +193,7 @@ public class BombermanMod extends Plugin{
 
         public static Powerup wall(Block block){
             for(Powerup powerup : values()){
-                if(powerup.block == block) return powerup;
+                if(powerup.block.block == block) return powerup;
             }
 
             return null;
