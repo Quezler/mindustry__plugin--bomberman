a0116c3b1964ef217375631cfddfc841901a6517
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-21 22:12:08 +0100

    Get rid of size

--- src/main/java/bomberman/Bomberman.java
+++ src/main/java/bomberman/Bomberman.java
@@ -14,7 +14,6 @@ import static mindustry.Vars.world;
 // constants and some utility functions
 public class Bomberman{
     public final static int grid = 15; // odd
-    public final static int size = (grid * 3);
     public final static String mapname = "[royal]Bomberman [white]\uF831";
     public static Slate[][] slates = new Slate[grid][grid];
 
