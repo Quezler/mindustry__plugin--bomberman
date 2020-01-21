1ab9f6db3be72078919cd89a8e29c8538ee95314
Patrick 'Quezler' Mounier <patrick@queztech.net>
2020-01-21 20:15:33 +0100

    Expand pallete total to 5

--- src/main/java/bomberman/Pallete.java
+++ src/main/java/bomberman/Pallete.java
@@ -5,7 +5,11 @@ import mindustry.world.*;
 import mindustry.world.blocks.*;
 
 public enum Pallete{
-    sandy(Blocks.darksand, Blocks.duneRocks, Blocks.scrapWallHuge, Blocks.liquidVoid);
+    cavern(Blocks.darksand, Blocks.duneRocks, Blocks.scrapWallHuge, Blocks.liquidVoid),
+    spored(Blocks.shale, Blocks.sporerocks, Blocks.oilExtractor, Blocks.sporePine),
+    desert(Blocks.sand, Blocks.saltRocks, Blocks.largeSolarPanel, Blocks.sandBoulder),
+    forged(Blocks.hotrock, Blocks.cliffs, Blocks.multiPress, Blocks.rock),
+    chanel(Blocks.water, Blocks.darkMetal, Blocks.liquidTank, Blocks.oreTitanium);
 
     public final Floor floor;
     public final StaticWall wall;
